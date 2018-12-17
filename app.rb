7da420ae0a3c62db1cef90ffa0adead1402623be
Mohammed Noori <kmanoorib@gmail.com>
2018-12-17 21:55:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,9 +3,16 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   
+  # Accepts a name and renders the name backwards
   get "/reversename/:name" do
     @name = params[:name]
     "#{name.reverse}"
+  end
+  
+  # Accepts a number and returns the square of that number
+  get "/square/:number" do
+    @num = params[:number]
+    "#{@num * @num}"
   end
   
 end
